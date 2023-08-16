4ada5d577b0c07c42d625315599ff8036b6e9ecc
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2023-08-16 15:25:46 +0400

    Added a function to check if the field is empty.

--- book.js
+++ book.js
@@ -1,11 +1,27 @@
-//gamoitane info button
-//if empty dont let them submit
-
 let button = document.getElementById("submit");
 
 button.addEventListener("click", function () {
   let name = document.querySelector("#customerName").value;
+  if (typeof name === "string" && name.length === 0) {
+    console.log("ad");
+  }
   let lastName = document.querySelector("#customerLastname").value;
+  if (typeof name === "string" && name.length === 0) {
+    console.log("ba");
+  }
   let number = document.querySelector("#customerNumber").value;
-  console.log(name, lastName, number);
+  if (typeof number === "string" && number.length === 0) {
+    console.log("ad");
+  }
+  let date = document.querySelector("#customerDate").value;
+  if (typeof date === "string" && date.length === 0) {
+    console.log("ad");
+  }
+  let time = document.querySelector("#customerTime").value;
+  if (typeof time === "string" && time.length === 0) {
+    console.log("ad");
+  }
+  alert(`Dearest ${name} ${lastName}, you just successfully booked a table for ${date} at 
+  ${time}! If anything changes on our side, we will contact you via your number: ${number}.
+  `);
 });
