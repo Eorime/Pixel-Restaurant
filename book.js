86d4c54867753d2d0b04ec97de668675b856d732
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2023-08-16 15:41:27 +0400

    Further fixes!

--- book.js
+++ book.js
@@ -7,15 +7,15 @@ button.addEventListener("click", function () {
   let date = document.querySelector("#customerDate").value;
   let time = document.querySelector("#customerTime").value;
   if (typeof name === "string" && name.length === 0) {
-    console.log("ad");
-  } else if (typeof name === "string" && name.length === 0) {
-    console.log("ba");
+    alert("Please put in your name!");
+  } else if (typeof lastName === "string" && lastName.length === 0) {
+    alert("Please put in your last name!");
   } else if (typeof number === "string" && number.length === 0) {
-    console.log("ad");
+    alert("Don't forget your number!");
   } else if (typeof date === "string" && date.length === 0) {
-    console.log("ad");
+    alert("What about the date?");
   } else if (typeof time === "string" && time.length === 0) {
-    console.log("ad");
+    alert("What time?");
   } else
     alert(`Dearest ${name} ${lastName}, you just successfully booked a table for ${date} at 
   ${time}! If anything changes on our side, we will contact you via your number: ${number}.
